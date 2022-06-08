1e31f02d5e9b85efe75390041abd4b1a8b9efb60
LAPTOP-H6RCKGQ2\xwb <wenbinxiao1997@gmail.com>
2022-06-09 01:07:49 +0800

    feat:go 闭包

---
+++ src/ch11function/anonymousFunc.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 将匿名函数保存到变量
+	add := func(x, y int) {
+		fmt.Println(x + y)
+	}
+	add(10, 20) // 通过变量调用匿名函数
+
+	//自执行函数：匿名函数定义完加()直接执行
+	func(x, y int) {
+		fmt.Println(x + y)
+	}(10, 20)
+}
---
+++ src/ch11function/biBao.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+//闭包
+func adder() func(int) int {
+	var x int
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+func main() {
+	var f = adder()
+	fmt.Println(f(10)) //10
+	fmt.Println(f(20)) //30
+	fmt.Println(f(30)) //60
+
+	f1 := adder()
+	fmt.Println(f1(40)) //40
+	fmt.Println(f1(50)) //90
+}
