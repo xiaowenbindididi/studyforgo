1f2818e457d4e740b491d24b8d1ca26dae5ce7f9
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-06-15 20:52:04 +0800

    feat:fuck package

--- src/ch12pachage/testPackage.go
+++ src/ch12pachage/testPackage.go
@@ -1,9 +1,12 @@
 package main
 //当你的代码都放在gopath目录下，我们导入包要从gopath下的src后面继续写起
-import ""
+import (
+	calc "ch12pachage/calc"
+	"fmt"
+)
 
 func main() {
 	var a int = 10
 	var b int = 20
-
+	fmt.Println
 }
